42fa86e451f7fd6e3548c102719216d5d19c4f4b
Chardon <wowdepletedl@gmail.com>
2019-06-01 20:23:35 +0200

    menu ok

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Link
+
+https://wowdepleted.github.io/Depleted/
--- script/pageGenerator.js
+++ script/pageGenerator.js
@@ -1,11 +1,44 @@
+function generateMenu(data){
+	var tagDropdown = $("<li/>").addClass("dropdown");
+	var tagDropbtn = $("<a/>").addClass("dropbtn");
+	var tagDropdownContent = $("<div/>").addClass("dropdown-content");
+	
+	var divMainMenu=$("#navigation-menu");
+	divMainMenu.append("<ul></ul>");
+	var ul = divMainMenu.find("ul");
+	$.each( data, function( raid, val ) {
+		ul.append(tagDropdown.clone());
+		var li = ul.find("li").last();
+		
+		tagDropbtn.html(val["RaidRealName"]);
+		li.append(tagDropbtn.clone())
+		li.append(tagDropdownContent.clone());
+		var dropdown = li.find("div");
+		
+		console.log(val["Boss"]);
+		$.each( val["Boss"], function( boss, bossValues ) {
+			dropdown.append("<a href=\"#\">"+bossValues["BossRealName"]+"</a>");
+		});
+	});
+}
+
+function generateIndexPage(data){}
+
 function generatePageFromJson(){
 	var divMainContent=$("#main-content");
+	
 	console.log(divMainContent);
 	
-	$.getJSON( "./json.json", function( data ) {
-		$.each( data, function( key, val ) {
-			console.log(key + " - " + val);
-		});
+	var jsonLink = "https://raw.githubusercontent.com/WowDepleted/Depleted/master/json.json"
+	
+	$.getJSON( jsonLink, function( data ) {
+		generateMenu(data);
+		if(pageName==="index"){
+			generateIndexPage(data);
+		}
+		else{
+			console.log("TODO: boss strat page");
+		}
 	});
 }
 
