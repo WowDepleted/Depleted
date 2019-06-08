59f9dee5aa41e6d3a0246648af001c7ac984de3a
Chardon <wowdepletedl@gmail.com>
2019-06-08 20:05:37 +0200

    quick update

--- script/pageGenerator.js
+++ script/pageGenerator.js
@@ -1,21 +1,49 @@
+var jsonLink = "https://raw.githubusercontent.com/WowDepleted/Depleted/master/json.json"
+
+function reloadPage(){
+	cleanPage();
+	generatePageFromJson(false);
+}
+
+function cleanPage(){
+	$("#main-content").text("");
+}
+
+
+function getCurrentPage(){
+	var url = window.location.href;
+	console.log(url);
+	if (url.indexOf('#') == -1) {
+		return "index";
+	}
+	var args = url.split("#")[1];
+	if(args.length ===0){
+		return "index";
+	}
+	
+	
+	console.log(args);
+}
+
 function generateMenu(data){
 	var tagDropdown = $("<li/>").addClass("dropdown");
-	var tagDropbtn = $("<a/>").addClass("dropbtn");
+	var tagDropbtn = $("<a/>").addClass("dropbtn").click(reloadPage);
 	var tagDropdownContent = $("<div/>").addClass("dropdown-content");
 	
 	var divMainMenu=$("#navigation-menu");
 	divMainMenu.append("<ul></ul>");
 	var ul = divMainMenu.find("ul");
-	$.each( data, function( raid, val ) {
+	$.each( data, function( raid, raidVal ) {
 		ul.append(tagDropdown.clone());
 		var li = ul.find("li").last();
 		
-		tagDropbtn.html(val["RaidRealName"]);
+		tagDropbtn.html(raidVal["RaidRealName"]);
 		li.append(tagDropbtn.clone())
 		li.append(tagDropdownContent.clone());
 		var dropdown = li.find("div");
-		$.each( val["Boss"], function( boss, bossValues ) {
-			dropdown.append("<a href=\"#\">"+bossValues["BossRealName"]+"</a>");
+		$.each( raidVal["Boss"], function( boss, bossVal ) {
+			var link = "#raid="+raid+"&boss="+bossVal["BossCode"];
+			dropdown.append("<a href=\""+link+"\">"+bossVal["BossRealName"]+"</a>");
 		});
 	});
 }
@@ -24,7 +52,7 @@ function generateIndexPage(data){
 	var divMainContent=$("#main-content");
 	var gallery = $("<div/>").attr("id","gallery");
 	divMainContent.append(gallery);
-	var contentBox = $("<div/>");
+	var contentBox = $("<a/>").click(reloadPage);
 	var name = $("<div/>") ;
 	var picture = $("<img/>");
 	$.each( data, function( raid, raidData ) {
@@ -34,15 +62,22 @@ function generateIndexPage(data){
 		var currentBox = contentBox.clone()
 							.append(raidPicture)
 							.append(raidName);
+		currentBox.attr("href","#raid="+raid);
 		gallery.append(currentBox);
 	});
 }
 
-function generatePageFromJson(){
-	var jsonLink = "https://raw.githubusercontent.com/WowDepleted/Depleted/master/json.json"
+function generateReloadOnClick(){
+
+
+}
+
+function generatePageFromJson(menu=true){
+	var pageName = getCurrentPage()
 	
 	$.getJSON( jsonLink, function( data ) {
-		generateMenu(data);
+		if(menu)
+			generateMenu(data);
 		if(pageName==="index"){
 			generateIndexPage(data);
 		}
@@ -50,8 +85,15 @@ function generatePageFromJson(){
 			console.log("TODO: boss strat page");
 		}
 	});
+	
+	generateReloadOnClick();
 }
 
+
 $(document).ready(function(){
-	generatePageFromJson();
+	generatePageFromJson(true);
+});
+
+$(window).on('hashchange', function(e){
+	reloadPage();
 });
